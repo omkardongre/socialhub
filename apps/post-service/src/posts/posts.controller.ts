931aa52e86f7abc593bda6c9b3aa3830d79c215e
omkardongre <44136758+omkardongre@users.noreply.github.com>
2025-05-04 12:01:24 +0530

    feat(post-service): implement create/read posts endpoints, DTO, service, controller, and PrismaService refactor (Day 32)

--- apps/post-service/src/posts/posts.controller.ts
+++ apps/post-service/src/posts/posts.controller.ts
@@ -1,7 +1,21 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Post, Body, Get, Param } from '@nestjs/common';
+import { PostsService } from './posts.service';
+import { CreatePostDto } from './dto/create-post.dto';
 
 @Controller('posts')
 export class PostsController {
+  constructor(private readonly postsService: PostsService) {}
+
+  @Post()
+  create(@Body() createPostDto: CreatePostDto) {
+    return this.postsService.create(createPostDto);
+  }
+
+  @Get('user/:id')
+  findByUser(@Param('id') userId: string) {
+    return this.postsService.findByUser(userId);
+  }
+
   @Get('ping')
   ping() {
     return { message: 'Post Service is live!' };
--- apps/post-service/src/posts/posts.service.ts
+++ apps/post-service/src/posts/posts.service.ts
@@ -1,4 +1,23 @@
 import { Injectable } from '@nestjs/common';
+import { CreatePostDto } from './dto/create-post.dto';
+import { PrismaService } from '../prisma/prisma.service';
 
 @Injectable()
-export class PostsService {}
+export class PostsService {
+  constructor(private prisma: PrismaService) {}
+
+  create(createPostDto: CreatePostDto) {
+    return this.prisma.post.create({
+      data: {
+        ...createPostDto,
+      },
+    });
+  }
+
+  findByUser(userId: string) {
+    return this.prisma.post.findMany({
+      where: { userId },
+      orderBy: { createdAt: 'desc' },
+    });
+  }
+}
